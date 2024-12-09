aa8ba9c0e03ae318833c60ab788a6d63fb494e63
AlexyBeaulieu <beaa6878@usherbrooke.ca>
2024-12-08 21:33:56 -0500

    ajout de l'intro

--- UnityProjetFinal/Assets/Scripts/GameOverManager.cs
+++ UnityProjetFinal/Assets/Scripts/GameOverManager.cs
@@ -7,6 +7,17 @@ public class GameOverManager : MonoBehaviour
 {
     public void ReturnToMainMenu()
     {
-        SceneManager.LoadScene("MainMenu"); 
+        /*
+        Scene currentScene = SceneManager.GetActiveScene ();
+        string sceneName = currentScene.name;
+
+        if(sceneName == "Niveau1")
+            SceneManager.LoadScene("Niveau1"); 
+        if(sceneName == "Niveau2")
+            SceneManager.LoadScene("Niveau2");
+        if(sceneName == "Niveau3")
+            SceneManager.LoadScene("Niveau3");
+        */
+        SceneManager.LoadScene("MainMenu");
     }
 }
